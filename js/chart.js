6cabc5a2c88a9ac7f4dac3660aa1d74d8be9f865
adamrotal <adamrotal@gmail.com>
2018-05-05 18:20:39 +0700

    memperbaiki perbaikan yg perlu diperbaiki

--- js/chart.js
+++ js/chart.js
@@ -1,17 +1,32 @@
 google.charts.load('current', {'packages':['corechart', 'line']});
-google.charts.setOnLoadCallback(drawOverallLineChart);
+google.charts.setOnLoadCallback(drawOverallLineChartHK);
 
-function drawOverallLineChart() {
+google.charts.load('current', {'packages':['corechart', 'line']});
+google.charts.setOnLoadCallback(drawLineChartHK);
+
+$(window).load(function () {
+	$('select[name=selector]').change(function(){
+		if ($(this).val() == 'HK') {
+			google.charts.setOnLoadCallback(drawLineChartHK);
+			google.charts.setOnLoadCallback(drawOverallLineChartHK);
+	    } else if ($(this).val() == 'JP') {
+	    	google.charts.setOnLoadCallback(drawLineChartJP);
+	    	google.charts.setOnLoadCallback(drawOverallLineChartJP);
+	    } else if ($(this).val() == 'MY') {
+	    	google.charts.setOnLoadCallback(drawLineChartMY);
+	    	google.charts.setOnLoadCallback(drawOverallLineChartMY);
+	    } else if ($(this).val() == 'SG') {
+	    	google.charts.setOnLoadCallback(drawLineChartSG);
+	    	google.charts.setOnLoadCallback(drawOverallLineChartSG);
+	    } else if ($(this).val() == 'TH') {
+	    	google.charts.setOnLoadCallback(drawLineChartTH);
+	    	google.charts.setOnLoadCallback(drawOverallLineChartTH);
+	    }
+	});
+});
+
+function drawOverallLineChartTH() {
 	var query = new google.visualization.Query(
-		// Hong Kong
-//		'https://docs.google.com/spreadsheets/d/19CBoYr-TuikOVTLTM9TpIR0-UMI04cs5ggZJwpf7DG4/gviz/tq?sheet=AllHongkong&headers=1&tq?range=A:F');
-		// Japan
-//		'https://docs.google.com/spreadsheets/d/1soezodPX4_EUsa3yg9wW5EoP090io53a2W7_p6OPBwo/gviz/tq?sheet=AllJapan&headers=1&tq?range=A:F');
-		// Malaysia
-//		'https://docs.google.com/spreadsheets/d/1RYinAHvRbd4UWn2RjUHpIMootzs_0qf3SGMJoY-CEdo/gviz/tq?sheet=AllMalaysia&headers=1&tq?range=A:F');
-		// Singapore
-//		'https://docs.google.com/spreadsheets/d/1V1zA9Lti89c2hAQsHiLWRZ_IyoxqWQ9pSan86-Ej9Nw/gviz/tq?sheet=AllSingapore&headers=1&tq?range=A:F');
-		// Thailand
 		'https://docs.google.com/spreadsheets/d/1YG9R5KGA4CBqGI7Uv69fqjiGO1t_VtYNMOG6naZNUsM/gviz/tq?sheet=AllThailand&headers=1&tq?range=A:F');
 
 	query.send(function(response) {
@@ -23,40 +38,154 @@ function drawOverallLineChart() {
 		var data = response.getDataTable();
 		var chart = new google.visualization.LineChart(document.getElementById('linechart_material'));
 		var options = {
+			legend: {
+				position: 'top',
+				alignment: 'center'
+			},
 			hAxis: {
 				title: 'Week'
 			},
 			vAxis: {
 				title: 'Search Trend'
 			}
+
+			// lineWidth: 10
 		};
 
 		chart.draw(data, options);
 	});
 }
 
+function drawOverallLineChartHK() {
+	var query = new google.visualization.Query(
+		// Hong Kong
+		'https://docs.google.com/spreadsheets/d/19CBoYr-TuikOVTLTM9TpIR0-UMI04cs5ggZJwpf7DG4/gviz/tq?sheet=AllHongkong&headers=1&tq?range=A:F');
 
-google.charts.load('current', {'packages':['corechart', 'line']});
-google.charts.setOnLoadCallback(drawLineChartHK);
+	query.send(function(response) {
+		if (response.isError()) {
+			alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+			return;
+		}
 
-$(window).load(function () {
-	$('select[name=selector]').change(function(){
-		if ($(this).val() == 'HK') {
-			google.charts.setOnLoadCallback(drawLineChartHK);
-	    } else if ($(this).val() == 'JP') {
-	    	google.charts.setOnLoadCallback(drawLineChartJP);
-	    } else if ($(this).val() == 'MY') {
-	    	google.charts.setOnLoadCallback(drawLineChartMY);
-	    } else if ($(this).val() == 'SG') {
-	    	google.charts.setOnLoadCallback(drawLineChartSG);
-	    } else if ($(this).val() == 'TH') {
-	    	google.charts.setOnLoadCallback(drawLineChartTH);
-	    }
+		var data = response.getDataTable();
+		var chart = new google.visualization.LineChart(document.getElementById('linechart_material'));
+		var options = {
+			legend: {
+				position: 'top',
+				alignment: 'center'
+			},
+			hAxis: {
+				title: 'Week'
+			},
+			vAxis: {
+				title: 'Search Trend'
+			}
+
+			// lineWidth: 10
+		};
+
+		chart.draw(data, options);
 	});
-});
+}
+
+function drawOverallLineChartJP() {
+	var query = new google.visualization.Query(
+		// Japan
+		'https://docs.google.com/spreadsheets/d/1soezodPX4_EUsa3yg9wW5EoP090io53a2W7_p6OPBwo/gviz/tq?sheet=AllJapan&headers=1&tq?range=A:F');
+
+	query.send(function(response) {
+		if (response.isError()) {
+			alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+			return;
+		}
+
+		var data = response.getDataTable();
+		var chart = new google.visualization.LineChart(document.getElementById('linechart_material'));
+		var options = {
+			legend: {
+				position: 'top',
+				alignment: 'center'
+			},
+			hAxis: {
+				title: 'Week'
+			},
+			vAxis: {
+				title: 'Search Trend'
+			}
+
+			// lineWidth: 10
+		};
+
+		chart.draw(data, options);
+	});
+}
+
+function drawOverallLineChartMY() {
+	var query = new google.visualization.Query(
+		// Malaysia
+		'https://docs.google.com/spreadsheets/d/1RYinAHvRbd4UWn2RjUHpIMootzs_0qf3SGMJoY-CEdo/gviz/tq?sheet=AllMalaysia&headers=1&tq?range=A:F');
+
+	query.send(function(response) {
+		if (response.isError()) {
+			alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+			return;
+		}
+
+		var data = response.getDataTable();
+		var chart = new google.visualization.LineChart(document.getElementById('linechart_material'));
+		var options = {
+			legend: {
+				position: 'top',
+				alignment: 'center'
+			},
+			hAxis: {
+				title: 'Week'
+			},
+			vAxis: {
+				title: 'Search Trend'
+			}
+
+			// lineWidth: 10
+		};
+
+		chart.draw(data, options);
+	});
+}
+
+function drawOverallLineChartSG() {
+	var query = new google.visualization.Query(
+		// Singapore
+		'https://docs.google.com/spreadsheets/d/1V1zA9Lti89c2hAQsHiLWRZ_IyoxqWQ9pSan86-Ej9Nw/gviz/tq?sheet=AllSingapore&headers=1&tq?range=A:F');
+
+	query.send(function(response) {
+		if (response.isError()) {
+			alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+			return;
+		}
+
+		var data = response.getDataTable();
+		var chart = new google.visualization.LineChart(document.getElementById('linechart_material'));
+		var options = {
+			legend: {
+				position: 'top',
+				alignment: 'center'
+			},
+			hAxis: {
+				title: 'Week'
+			},
+			vAxis: {
+				title: 'Search Trend'
+			}
+
+			// lineWidth: 10
+		};
+
+		chart.draw(data, options);
+	});
+}
 
 function drawLineChartHK() {
-	console.log('hongkong');
+	// console.log('hongkong');
 	var queryString = encodeURIComponent('SELECT A, B');
 	
 	var hk_places = ['hongkong-disneyland', 'madame-tussauds', 'lantau-island', 'ocean-park', 'tsim-sha-tsui'];
@@ -72,7 +201,7 @@ function drawLineChartHK() {
 
 function drawLineChartJP() {
 
-	console.log('jap');
+	// console.log('jap');
 	var queryString = encodeURIComponent('SELECT A, B');
 	
 	var jp_places = ['arashiyama', 'mount-fuji', 'odaiba', 'tokyo-disneyland', 'universal-studios-jp'];
@@ -88,7 +217,7 @@ function drawLineChartJP() {
 
 function drawLineChartMY() {
 
-	console.log('may');
+	// console.log('may');
 	var queryString = encodeURIComponent('SELECT A, B');
 	
 	var my_places = ['batu-caves', 'legoland', 'petronas', 'penang-hill', 'sunway-lagoon'];
@@ -104,7 +233,7 @@ function drawLineChartMY() {
 
 function drawLineChartSG() {
 
-	console.log('sig');
+	// console.log('sig');
 	var queryString = encodeURIComponent('SELECT A, B');
 	
 	var sg_places = ['universal-studios-sg', 'merlion', 'bugis', 'gardens-by-the-bay', 'marina-bay-sands'];
@@ -120,7 +249,7 @@ function drawLineChartSG() {
 
 function drawLineChartTH() {
 
-	console.log('thai');
+	// console.log('thai');
 	var queryString = encodeURIComponent('SELECT A, B');
 	
 	var th_places = ['grand-palace', 'khaosan', 'khao-yai', 'mbk-center', 'phi-phi'];
@@ -153,6 +282,9 @@ function sendQuery(query, place) {
 		var options = {
 			// 'width':1100,
 			// 'height':140,
+			legend: {
+				position :'none'
+			},
 			hAxis: {
 				title: 'Week'
 			},
